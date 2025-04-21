186cf574ca7df7a17d781333719dc93cfbb8f6f9
guzmanzerouno <44323727+guzmanzerouno@users.noreply.github.com>
2025-04-21 17:41:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1828,7 +1828,7 @@ async function analyzeTransaction(signature, forceCheck = false) {
     const actionButtons = [
       [
         { text: "🔄 Refresh Info", callback_data: `refresh_${mint}` },
-        { text: "📊 Chart+Txns", url: `https://pumpultra.fun/solana/${mint}.html` }
+        { text: "📊 Chart+Txns", url: `https://gemsniping.com/solana/${mint}` }
       ],
       [
         { text: "💰 0.1 Sol", callback_data: `buy_${mint}_0.1` },
@@ -2113,7 +2113,7 @@ bot.onText(/\/autobuy/, async (msg) => {
           inline_keyboard: [
             [
                 { text: "🔄 Refresh Info", callback_data: `refresh_${mint}` },
-                { text: "📊 Chart+Txns", url: `https://pumpultra.fun/solana/${mint}.html` }
+                { text: "📊 Chart+Txns", url: `https://gemsniping.com/solana/${mint}` }
               ],
               [
                 { text: "💰 0.1 Sol", callback_data: `buy_${mint}_0.1` },
@@ -2633,7 +2633,7 @@ bot.on("callback_query", async (query) => {
             { text: "💯 Sell MAX", callback_data: `sell_${receivedTokenMint}_100` }
           ],
           [
-            { text: "📈 📊 Chart+Txns", url: `https://pumpultra.fun/solana/${receivedTokenMint}.html` }
+            { text: "📈 📊 Chart+Txns", url: `https://gemsniping.com/solana/${receivedTokenMint}` }
           ]
         ]
       }
@@ -2834,7 +2834,7 @@ async function refreshBuyConfirmationV2(chatId, messageId, tokenMint) {
               { text: "💯 Sell MAX",  callback_data: `sell_${tokenMint}_100` }
             ],
             [
-              { text: "📊 Chart+Txns", url: `https://pumpultra.fun/solana/${tokenMint}.html` }
+              { text: "📊 Chart+Txns", url: `https://gemsniping.com/solana/${tokenMint}` }
             ]
           ]
         }
